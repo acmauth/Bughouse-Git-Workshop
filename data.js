7b6ba84f7cd3ef92a5825080bc5d864fb3a0cc90
Poly <159016999+0x6B70@users.noreply.github.com>
2026-04-02 23:21:09 +0300

    .

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 const data=[
-  {"Name": "Konstantinos Pol", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
+  {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
 ];
 
