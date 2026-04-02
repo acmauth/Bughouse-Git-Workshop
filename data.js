9ae7651315bf1d69f56ad2a15103eaf22231faac
Poly <159016999+0x6B70@users.noreply.github.com>
2026-04-02 19:00:45 +0300

    .

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 const data=[
-  {"Name": "Konstantinos Pol", "Age": 20, "Hobbies": {"Table Tennis", "Chess"}, "Dream Trip Destination": "Japan"}
+  {"Name": "Konstantinos Pol", "Age": 20, "Dream Trip Destination": "Japan"},
 ];
 
