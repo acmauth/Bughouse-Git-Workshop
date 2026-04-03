15db822bae266ee18ac7647b8d66d7c80e059288
ChristosParaskevaidis <paraskevaidischristos2@gmail.com>
2026-04-03 18:28:33 +0300

    Add information for Christos!!!!!

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
+  {"Name": "Paraskevaidis Christos", "Age": 19 ,"Hobbies":["Video Games","Movies"],"Fun Fact": "Miso ton Giannh","Dream Trip Destination": "China"},
 ];
 
