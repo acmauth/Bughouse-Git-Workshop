1479b11a6565f65bbd2381ee4acb784d1b97f965
Poly <159016999+0x6B70@users.noreply.github.com>
2026-04-03 18:54:18 +0300

    Reorder entries in data array

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
-  {"Name": "Valentini Sidirpoulou", "Age": 20, "From": "Thessaloniki"},
  {"Name": "Amanetidou Giwta", "Age": 19 ,"Hobbies":["Reading","Gaming"],"Fun Fact": "I hate flwrou","Dream Trip Destination": "France"},
    {"Name": "Florou Eleni ", "Age": 19 ,"Hobbies":["Reading","Videogames"],"Fun Fact": "Nantia is sitting next to me rn","Dream Trip Destination": "Spain or Japan"},
   {"Name": "Doumpenidou Elena", "Age": 19, "Hobbies": "Music", "Dream Trip": "China (Again)"},
   {"Name": "Paraskevaidis Christos", "Age": 19 ,"Hobbies":["Video Games","Movies"],"Fun Fact": "Miso ton Giannh","Dream Trip Destination": "China"},
   {"Name": "Lioupis Themistoklis", "Age": 18 ,"Hobbies":["Video Games", "Basketball", "Beer!"],"Fun Fact": "Yes.","Dream Trip Destination": "Home"},
   {"Name": "Karachristos Giorgos", "Age": 18, "Hobbies":["Eating", "pingpong", "chess"], "Fun Fact": "Karditsa sucks worst place on earth volos solos", "Dream Trip Destination": "oxi karditsa"},
+    {"Name": "Valentini Sidirpoulou", "Age": 20, "From": "Thessaloniki"},
 ];
 
 
