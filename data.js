f8a61320f3045fb5b667dccf0c3b00b7306324b5
Gekaras <giorgoskarachrestos25@gmail.com>
2026-04-03 18:31:41 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
+  {"Name": "Karachristos Giorgos", "Age": 18, "Hobbies":["Eating", "pingpong", "chess"], "Fun Fact": "Karditsa sucks worst place on earth volos solos", "Dream Trip Destination": "oxi karditsa"},
 ];
 
