e1e007a9bdb784b16f68c404ccaa2561bb3efc75
ValentiniSidiropoulou <valesi2006@gmail.com>
2026-04-03 18:50:13 +0300

    Add new entry to data array in data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
-];
+  {"Name": "Valentini Sidirpoulou", "Age": 20, "From": "Thessaloniki"},];
 
