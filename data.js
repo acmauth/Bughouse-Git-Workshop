69c4563d08793cb3f78d660c43c487e0d66536bd
elenadb00 <doumpenel@icloud.com>
2026-04-03 18:35:56 +0300

    elena

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
+  {"Name": "Doumpenidou Elena", "Age": 19, "Hobbies": "Music", "Dream Trip": "China (Again)"},
 ];
 
