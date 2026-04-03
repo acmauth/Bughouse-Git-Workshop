ab3e3c73a96ebc60ef827d68b33c723a6097b064
Themis101 <lioupisthemistoklis@gmail.com>
2026-04-03 18:29:10 +0300

    Fix conflict yis

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 const data=[
+  {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
   {"Name": "Lioupis Themistoklis", "Age": 18 ,"Hobbies":["Video Games", "Basketball", "Beer!"],"Fun Fact": "Yes.","Dream Trip Destination": "Home"},
 ];
-
