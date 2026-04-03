69ff6a4476ef1966b22b19e303470f1c9b2aad9b
hgiwta <giotamanetidouu@gmail.com>
2026-04-03 18:34:40 +0300

    giwtabranch

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
+ {"Name": "Amanetidou Giwta", "Age": 19 ,"Hobbies":["Reading","Gaming"],"Fun Fact": "I hate flwrou","Dream Trip Destination": "France"},
 ];
 
