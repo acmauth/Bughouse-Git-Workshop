396eca99566351d4a9acabcd18be989ded7460c7
flwwrou <elenifloroucs@gmail.com>
2026-04-03 18:34:34 +0300

    eleni

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
 const data=[
   {"Name": "Polychronopoulos Konstantinos", "Age": 20 ,"Hobbies":["Table Tennis","Chess"],"Fun Fact": "I grew up in Karditsa but don't call me Karditsioti","Dream Trip Destination": "Japan"},
+   {"Name": "Florou Eleni ", "Age": 19 ,"Hobbies":["Reading","Videogames"],"Fun Fact": "Nantia is sitting next to me rn","Dream Trip Destination": "Spain or Japan"},
 ];
+
 
